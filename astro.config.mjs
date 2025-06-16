a354181f86040ccd4457d60a38eb80db0d5a29aa
cyz <122550888@qq.com>
2025-06-16 22:27:39 +0800

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,8 +21,8 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
-  base: "/",
+  site: "https://github.com/122550888/",
+  base: "/blog",
   trailingSlash: "always",
   integrations: [
     tailwind(
